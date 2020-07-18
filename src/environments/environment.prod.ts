0a32168c2b15956695f071b1b504cc25b2692e07
mkmishra8191 <manjeet.eng8191@gail.com>
2020-07-18 07:18:30 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCa9QbpdxpSCbCasX0ZR_gXGFO0uoIT2mo",
+    authDomain: "mytodo-2ea8a.firebaseapp.com",
+    databaseURL: "https://mytodo-2ea8a.firebaseio.com",
+    projectId: "mytodo-2ea8a",
+    storageBucket: "mytodo-2ea8a.appspot.com",
+    messagingSenderId: "93390643089",
+    appId: "1:93390643089:web:9f0a5e7ba7369406c64430",
+    measurementId: "G-J7NWMD3ZML"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCa9QbpdxpSCbCasX0ZR_gXGFO0uoIT2mo",
+    authDomain: "mytodo-2ea8a.firebaseapp.com",
+    databaseURL: "https://mytodo-2ea8a.firebaseio.com",
+    projectId: "mytodo-2ea8a",
+    storageBucket: "mytodo-2ea8a.appspot.com",
+    messagingSenderId: "93390643089",
+    appId: "1:93390643089:web:9f0a5e7ba7369406c64430",
+    measurementId: "G-J7NWMD3ZML"
+  }
 };
 
 /*
